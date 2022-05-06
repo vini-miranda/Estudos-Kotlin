6e38722d8455aafbe0e26f96c34168b651f357bb
Vinicius Miranda <v-alves-miranda@live.com>
2022-05-06 11:55:24 -0300

    Variáveis

--- byteBank/src/main/kotlin/main.kt
+++ byteBank/src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main(){
-    print("Bem vindo ao ByteBank")
+    println("Bem vindo ao ByteBank")
+    val titular = "Vinicius Miranda" //variável imutável
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 300.0
+
+    println("Titular: $titular")
+    println("Numero da conta: $numeroConta")
+    println("Saldo: R$$saldo")
+
 }
