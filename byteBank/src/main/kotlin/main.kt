cb49c0d71871cb3535d1eba103333e42749b50ba
Vinicius Miranda <v-alves-miranda@live.com>
2022-05-12 10:56:47 -0300

    Trabalhando com POO

--- byteBank/src/main/kotlin/main.kt
+++ byteBank/src/main/kotlin/main.kt
@@ -1,18 +1,30 @@
-fun main(){
+fun main() {
     println("Bem vindo ao ByteBank")
-    val conta = Conta()
-    conta.titular = "Vini"
-    println(conta.titular)
+    val conta1 = Conta()
+    conta1.titular = "Vini"
+    conta1.saldo = 10.0
+    val conta2 = Conta()
+    conta2.titular = "Kiss"
+
+    println("TITULAR: " + conta1.titular)
+    println("SALDO: R$" + conta1.saldo)
+    conta1.deposita(50.0)
+    println("SALDO: R$" + conta1.saldo)
 }
 
-class Conta{
+class Conta {
     var titular = ""
     var numero = 0
     var saldo = 0.0
+
+    fun deposita(valor: Double){
+        println("Deposito efetuado!")
+        this.saldo += valor
+    }
 }
 
-fun testaLaco(){
-    for (i in 1..5){
+fun testaLaco() {
+    for (i in 1..5) {
         val titular = "Vinicius Miranda" //variável imutável
         val numeroConta = 1000 + i
         var saldo = 0.0 + i
@@ -24,7 +36,8 @@ fun testaLaco(){
     }
 
 }
-fun testaCondicao(saldo: Double){
+
+fun testaCondicao(saldo: Double) {
     when {
         saldo > 0.0 -> println("Conta é positiva.")
 
